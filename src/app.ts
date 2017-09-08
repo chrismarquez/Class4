bfe3c625f22c0aca1c89b246d4b00406be761af4
christopher <a01225198@itesm.mx>
2017-09-08 16:01:56 -0500

    For and While Cycles

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+
+for (let i = 5; i >= 0; i--) {
+    console.log("Hello, several times: " + i);
+}
+
+for (let i = 1; i <= 4; i++) {
+    let line: string = "*";
+    line = line.repeat(i);
+    console.log(line);
+}
+
+let a = 1;
+
+while(a < 5) {
+    let line: string = "*";
+    line = line.repeat(a);
+    console.log(line);
+    a++;
+}
